71f75e69bc3d81cab88df7940612ae8b4f954796
litresperminute <70461497+litresperminute@users.noreply.github.com>
2025-01-28 14:35:57 -0700

    More branch testing

--- Thermal_Draw_Tower_Controller/Test/Test.ino
+++ Thermal_Draw_Tower_Controller/Test/Test.ino
@@ -1,13 +1,8 @@
-// Looks like this worked!
-
 // Imagine sophisticated PID control scheme here
 
 void setup() {
-  // put your setup code here, to run once:
-  // Test push
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+//  blah b lah blah
 }
